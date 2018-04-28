5b284888010be943992eca699957130c4149e103
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-04-28 21:51:32 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -56,3 +56,20 @@ TEST_CASE("builder_3")
   
   REQUIRE(result1 == result2);
 }
+
+TEST_CASE("insert")
+{
+  Tree tree = {12, 4, 56, 78, 2};
+  tree.insert(5);
+  std::ofstream out_file("file.txt");
+  
+  tree.save("file.txt");
+  out_file.close();
+  
+  std::string result1;
+  std::string result2 = "12 4 2 5 56 78 ";
+  std::ifstream in_file("file.txt");
+  getline(in_file, result1);
+  
+  REQUIRE(result1 == result2);
+}

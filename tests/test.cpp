39fbec5623497c3674d502edcd51958c5d094a97
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2018-04-28 21:03:21 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -17,15 +17,15 @@ TEST_CASE("builder_1")
 TEST_CASE("builder_2")
 {
   Tree tree = {12, 4, 56, 78, 2, 5};
-  std::ofstream file("file.txt");
+  std::ofstream out_file("file.txt");
   
   tree.save("file.txt");
-  file.close();
+  out_file.close();
   
   std::string result1;
   std::string result2 = "12 4 2 5 56 78 ";
-  std::ifstream file("file.txt");
-  getline(file, result1);
+  std::ifstream in_file("file.txt");
+  getline(in_file, result1);
   
   REQUIRE(result1 == result2);
 }
@@ -34,25 +34,25 @@ TEST_CASE("builder_3")
 {
   Tree tree1 = {12, 4, 56, 78, 2, 5};
   Tree tree2 = tree1;
-  std::ofstream file("file.txt");
+  std::ofstream out_file("file.txt");
   
   tree1.save("file.txt");
-  file.close();
+  out_file.close();
   
   std::string result1;
   
-  std::ifstream file("file.txt");
-  getline(file, result1);
-  file.close();
-  std::ofstream file1("file.txt");
+  std::ifstream in_file("file.txt");
+  getline(in_file, result1);
+  in_file.close();
+  std::ofstream out_file1("file.txt");
   tree2.save("file.txt");
-  file1.close();
+  out_file1.close();
   
   std::string result2;
   
-  std::ifstream file1("file.txt");
-  getline(file1, result2);
-  file1.close();
+  std::ifstream in_file1("file.txt");
+  getline(in_file1, result2);
+  in_file1.close();
   
   REQUIRE(result1 == result2);
 }
